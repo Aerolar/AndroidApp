97355e0176f264ece4639913e01f19bfe3b13a69
Ashish <ashishthapa53@yahoo.com>
2020-02-03 16:50:42 +0545

    homework

--- app/src/main/java/com/example/mytext/DashboardActivity.java
+++ app/src/main/java/com/example/mytext/DashboardActivity.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    Button btnArithmetic,btnArea,btnSI;
+    Button btnArithmetic,btnArea,btnSI,btnCheckArmstrong,btnPrime,btnReverse,btnSwap,btnAutomorphic,btnOddEven;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,12 @@ public class DashboardActivity extends AppCompatActivity {
         btnArea = findViewById(R.id.btnArea);
         btnArithmetic = findViewById(R.id.btnArithemtic);
         btnSI=findViewById(R.id.btnSI);
+        btnCheckArmstrong=findViewById(R.id.btnCheckArmstrong);
+        btnAutomorphic=findViewById(R.id.btnAutomorphic);
+        btnPrime=findViewById(R.id.btnPrime);
+        btnOddEven=findViewById(R.id.btnOddEven);
+        btnReverse=findViewById(R.id.btnReverse);
+        btnSwap=findViewById(R.id.btnSwap);
 
         btnArea.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +48,54 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(DashboardActivity.this,SimpleInterestActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCheckArmstrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,ArmstrongActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAutomorphic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,AutomorphicActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPrime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,PrimeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnOddEven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,OddEvenActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnReverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,ReverseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSwap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DashboardActivity.this,SwapActivity.class);
                 startActivity(intent);
             }
         });
